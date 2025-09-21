dca854e9e7dcf2df52fa0717dd065f9bd8bf9092
Harshith Rao <harshithrao316@gmail.com>
2025-09-21 13:19:43 +0530

    updated session management to stateless

--- src/main/java/com/harshith/dsa_question_picker/security/SecurityConfig.java
+++ src/main/java/com/harshith/dsa_question_picker/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -50,6 +51,9 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> {
                 })
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/public/**", "/oauth2/**", "/login/oauth2/**").permitAll()
                         .anyRequest().authenticated()
